08a9b2e7621b3716a8331fafe6b8a5521f210748
Marina Ismailova <marina1988sherbakova@gmail.com>
2020-06-17 22:40:25 -0400

    #2: [UI/UX]: add icons to the app

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # Technical screening
 Task - Implement the interface using HTML/CSS and Angular.
 
-# Description  
+## Description  
 **[Live Demo](http://mariskas.github.io/)** - here is a build version of the test task. 
 
 **Technologies:** Angular 8, HTML/Scss, Typescript, HTML/Scss, JavaScript.  
 
 **Tools:** Webstorm, Git, GitHub.
 
-# Comments
+**Additional packages:** [angular-svg-icon](https://github.com/czeckd/angular-svg-icon)
+
+## Comments
 ### Within the given time to complete the task, I did:
 1. Reusable components such as Buttons and Dropdown.
 1. Responsive for all components except the `table` .  
@@ -21,5 +23,3 @@ Aria label and roles added. But for the full implementation of ADA,
 for example, in dropdowns - Application need's an additional functionality 
 using [@angular/cdk](https://github.com/angular/components#readme) 
 ([dropdown example](https://stackblitz.com/edit/angular-custom-dropdown-cdk?file=src%2Fapp%2Fcustom-dropdown%2Fcustom-select.html)).
-
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { ButtonComponent } from './components/button/button.component';
-import { DropdownComponent } from './components/dropdown/dropdown.component';
-import { SearchComponent } from './components/search/search.component';
-import { TableComponent } from './components/table/table.component';
-import { PaginationComponent } from './components/table/components/pagination/pagination.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule} from "@angular/forms";
+
+import {AppComponent} from './app.component';
+import {ButtonComponent} from './components/button/button.component';
+import {DropdownComponent} from './components/dropdown/dropdown.component';
+import {SearchComponent} from './components/search/search.component';
+import {TableComponent} from './components/table/table.component';
+import {PaginationComponent} from './components/table/components/pagination/pagination.component';
+
+import {HttpClientModule} from "@angular/common/http";
+import {AngularSvgIconModule} from "angular-svg-icon";
+import { SortComponent } from './components/table/components/sort/sort.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +20,17 @@ import {FormsModule} from "@angular/forms";
     DropdownComponent,
     SearchComponent,
     TableComponent,
-    PaginationComponent
+    PaginationComponent,
+    SortComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    AngularSvgIconModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
