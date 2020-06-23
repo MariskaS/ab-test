3a7931ab8d1bf773d18bc7ca2d45800dbbac0a55
Marina Ismailova <marina1988sherbakova@gmail.com>
2020-06-22 22:26:18 -0400

    [UI/UX] add dark mode #23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, HostBinding} from '@angular/core';
+
+import {THEME} from "./constants";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  {
+  @HostBinding('attr.theme')
+  private theme = THEME.dark;
 
+  toggleTheme($event: boolean) {
+    if ($event === true) {
+      this.theme = THEME.light;
+    } else {
+      this.theme = THEME.dark;
+    }
+  }
 }
